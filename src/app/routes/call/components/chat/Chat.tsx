8624701d264b12c8c7f27ae6327e630dabc40970
YKFD <chervonchuk316@gmail.com>
2021-06-24 00:58:47 +0300

    fix on enter press

--- src/app/routes/call/components/chat/Chat.tsx
+++ src/app/routes/call/components/chat/Chat.tsx
@@ -11,7 +11,9 @@ export const Chat: React.FC = () => {
     const [userMessage, setUserMessage] = useState<string>("");
     const [cookies] = useCookies(["name"]);
     const {socket} = useSocket();
-    const messages = useSelector((state: {messages: Array<IMessage>}) => state.messages)
+    const messages = useSelector(
+        (state: { messages: Array<IMessage> }) => state.messages
+    );
 
     const sendMessage = useCallback(() => {
         socket.sendMessage({
@@ -26,19 +28,19 @@ export const Chat: React.FC = () => {
         <div className="chat-block">
             <div className="messages-block">
                 {[...messages]
-                  .sort((a, b) => b.date.valueOf() - a.date.valueOf())
-                  .map(({ username, message, date }) => (
-                    <div
-                      className="msg-block"
-                      key={`${username}-${message}-${date.valueOf()}}`}
-                    >
-                      <div className="msg-username">{username}</div>
-                      <div className="msg-message">{message}</div>
-                      <div className="msg-date">
-                        {moment(date).format("DD/MM/YYYY")}
-                      </div>
-                    </div>
-                  ))}
+                    .sort((a, b) => b.date.valueOf() - a.date.valueOf())
+                    .map(({username, message, date}) => (
+                        <div
+                            className="msg-block"
+                            key={`${username}-${message}-${date.valueOf()}}`}
+                        >
+                            <div className="msg-username">{username}</div>
+                            <div className="msg-message">{message}</div>
+                            <div className="msg-date">
+                                {moment(date).format("DD/MM/YYYY")}
+                            </div>
+                        </div>
+                    ))}
             </div>
             <div className="message-block">
                 <input
@@ -46,6 +48,7 @@ export const Chat: React.FC = () => {
                     type="text"
                     value={userMessage}
                     onChange={(e) => setUserMessage(e.target.value)}
+                    onKeyDown={(e) => (e.key === "Enter" ? sendMessage() : null)}
                     placeholder="Message..."
                 />
                 <button onClick={sendMessage} className="send-message-btn">
